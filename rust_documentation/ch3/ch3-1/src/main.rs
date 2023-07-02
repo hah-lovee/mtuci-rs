932a648a802e240cba059b951b5b1d3f5efd3ff7
hah-love <legyx1@gmail.com>
2023-07-02 15:24:49 +0300

    check shading

--- rust_documentation/ch3/ch3-1/src/main.rs
+++ rust_documentation/ch3/ch3-1/src/main.rs
@@ -1,10 +1,12 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
 fn main() {
+    let x = 5;
 
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-    println!("const {}", THREE_HOURS_IN_SECONDS)
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
