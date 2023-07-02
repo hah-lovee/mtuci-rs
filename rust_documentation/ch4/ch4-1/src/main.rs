90c95544ff2d83a13b6365b504d084bbe4ee59b9
hah-love <legyx1@gmail.com>
2023-07-02 18:59:43 +0300

    ownership with function 2.0

--- rust_documentation/ch4/ch4-1/src/main.rs
+++ rust_documentation/ch4/ch4-1/src/main.rs
@@ -1,21 +1,19 @@
 fn main() {
-    let s = String::from("hello"); 
+    let s1 = gives_ownership();         
 
-    let s = takes_ownership(s);             
+    let s2 = String::from("hello");     
 
-    let x = 5;                      
-    
-    makes_copy(x);     
+    let s3 = takes_and_gives_back(s2);  
+}
 
-    println!("main s -> {}", s);     
-    println!("main x -> {x}");      
+fn gives_ownership() -> String {             
 
-} 
-fn takes_ownership(some_string: String) -> String { 
-    println!("{}", some_string);
-    some_string
-} 
+    let some_string = String::from("yours"); 
+    some_string                              
+}
 
-fn makes_copy(some_integer: i32) { 
-    println!("{}", some_integer);
-} 
+
+fn takes_and_gives_back(a_string: String) -> String { 
+
+    a_string  
+}
