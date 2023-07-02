672a9d16efbf125356e9f72f55499defc4b2792d
hah-love <legyx1@gmail.com>
2023-07-02 19:36:25 +0300

    string slices

--- rust_documentation/ch4/ch4-3/src/main.rs
+++ rust_documentation/ch4/ch4-3/src/main.rs
@@ -1,19 +1,18 @@
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
 
-    s.len()
-}
+//     s.len()
+// }
 
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
-    let word = first_word(&s);
-    
-    s.clear();
+    let hello = &s[0..5];
+    let world = &s[6..11];
 }
