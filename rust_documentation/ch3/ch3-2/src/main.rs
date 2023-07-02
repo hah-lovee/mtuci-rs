a3913deefc8b84dd02c460dd7fb42ec1721b4205
hah-love <legyx1@gmail.com>
2023-07-02 16:16:22 +0300

    check type tuple 2.0

--- rust_documentation/ch3/ch3-2/src/main.rs
+++ rust_documentation/ch3/ch3-2/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let tup = (500, 6.4, 1);
+    let x: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x, y, z) = tup;
+    let five_hundred = x.0;
 
-    println!("The value of y is: {y}");
+    let six_point_four = x.1;
+
+    let one = x.2;
 }
