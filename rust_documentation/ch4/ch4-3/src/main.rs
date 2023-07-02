38df90e7fc8e8dd8b75c5e677ecd93fa72ff9f2f
hah-love <legyx1@gmail.com>
2023-07-02 19:44:52 +0300

    first word in string

--- rust_documentation/ch4/ch4-3/src/main.rs
+++ rust_documentation/ch4/ch4-3/src/main.rs
@@ -1,18 +1,21 @@
-// fn first_word(s: &String) -> usize {
-//     let bytes = s.as_bytes();
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
 
-//     for (i, &item) in bytes.iter().enumerate() {
-//         if item == b' ' {
-//             return i;
-//         }
-//     }
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
 
-//     s.len()
-// }
+    &s[..]
+}
 
 fn main() {
     let s = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    let word = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
 }
