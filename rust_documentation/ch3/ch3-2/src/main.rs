22a0092fbc321828eaece06634ca5df759fbe79e
hah-love <legyx1@gmail.com>
2023-07-02 16:04:12 +0300

    check type bool

--- rust_documentation/ch3/ch3-2/src/main.rs
+++ rust_documentation/ch3/ch3-2/src/main.rs
@@ -1,22 +1,5 @@
 fn main() {
-    // addition
-    let sum = 5 + 10;
+    let t = true;
 
-    println!("sum -> {sum}");
-    // subtraction
-    let difference = 95.5 - 4.3;
-    println!("difference -> {difference}");
-
-    // multiplication
-    let product = 4 * 30;
-    println!("product -> {product}");
-
-    // division
-    let quotient = 56.7 / 32.2;
-    println!("quotient -> {quotient}");
-    let truncated = -5 / 3; // Results in -1
-    println!("truncated -> {truncated}");
-    // remainder
-    let remainder = 43 % 5;
-    println!("remainder -> {remainder}");
-}  
+    let f: bool = false; 
+}
