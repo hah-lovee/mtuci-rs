2c2bb90b5646b5bef21e6cd8206d11b1af422176
hah-love <legyx1@gmail.com>
2023-07-02 16:06:10 +0300

    check type char

--- rust_documentation/ch3/ch3-2/src/main.rs
+++ rust_documentation/ch3/ch3-2/src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    let t = true;
-
-    let f: bool = false; 
+fn  main() {
+    let c = 'z';
+    println!("{c}");
+    let z: char = 'ℤ';
+    println!("{z}");
+    let heart_eyed_cat = '😻';
+    println!("{heart_eyed_cat}");
 }
