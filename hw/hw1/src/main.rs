cb7b1c3e4ac60359715f3b82ffbda507fdb6749f
hah-love <legyx1@gmail.com>
2023-07-03 13:47:12 +0300

    update decision

--- hw/hw1/src/main.rs
+++ hw/hw1/src/main.rs
@@ -1,14 +1,28 @@
-fn main() {
-    for i in 1..=100 {
+// fn main() {
+//     for i in 1..=100 {
 
-        if i%3 == 0 && i%5 == 0{
-            println!("FizzBuzz");
-        } else if i%3 == 0 {
-            println!("Fizz");
-        } else if i%5 == 0 {
-            println!("Buzz");
-        } else {println!("{i}")};
+//         if i%3 == 0 && i%5 == 0{
+//             println!("FizzBuzz");
+//         } else if i%3 == 0 {
+//             println!("Fizz");
+//         } else if i%5 == 0 {
+//             println!("Buzz");
+//         } else {println!("{i}")};
+//     }
+// }
+
+fn main () {
+    for i in 1..=100{
+
+        let mut answer = "".to_string();
+        if i % 3 == 0 {
+            answer.push_str("Fizz");
+        } if i % 5 == 0 {
+            answer.push_str("Buzz");
+        } if answer == "" {
+            answer.push_str(&i.to_string());
+        }
+        println!("{answer}");
     }
-
 
 }
