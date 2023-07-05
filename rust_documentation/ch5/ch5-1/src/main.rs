82e22747ded8f904f98525a805f84691ab45af66
hah-love <legyx1@gmail.com>
2023-07-05 22:41:24 +0300

    create tuple structures

--- rust_documentation/ch5/ch5-1/src/main.rs
+++ rust_documentation/ch5/ch5-1/src/main.rs
@@ -1,32 +1,47 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
-
-    // user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("anotheremail@example.com"),
-        ..user1
-    };
-
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+
+
+
+
+
+
+
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+
+// fn main() {
+//     let mut user1 = User {
+//         active: true,
+//         username: String::from("someusername123"),
+//         email: String::from("someone@example.com"),
+//         sign_in_count: 1,
+//     };
+
+//     // user1.email = String::from("anotheremail@example.com");
+
+//     let user2 = User {
+//         email: String::from("anotheremail@example.com"),
+//         ..user1
+//     };
+
+// }
+
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         active: true,
+//         username,
+//         email,
+//         sign_in_count: 1,
+//     }
+// }
