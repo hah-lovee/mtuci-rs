8f1d8539961d41f4c945792c4d41a219f42e475b
hah-love <legyx1@gmail.com>
2023-07-10 14:29:44 +0300

    print struct 3.0

--- rust_documentation/ch5/ch5-2/src/main.rs
+++ rust_documentation/ch5/ch5-2/src/main.rs
@@ -1,29 +1,15 @@
 #[derive(Debug)]
-struct Rectangle{
+struct Rectangle {
     width: u32,
     height: u32,
-} 
-
+}
 
 fn main() {
-    let rect1 = Rectangle{
-        width: 30,
-        height: 50
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
 
-    // let rect1 = (30, 50);
-    // let width1 = 30;
-    // let height1 = 50;
-
-    println!("rect is {:#?}", rect1)
-}    
-
-//     println!(
-//         "The area of the rectangle is {} square pixels.",
-//         area(&rect1)
-//     );
-// }
-
-// fn area(dimensions: &Rectangle) -> u32 {
-//     dimensions.width * dimensions.height
-// }
+    dbg!(&rect1);
+}
