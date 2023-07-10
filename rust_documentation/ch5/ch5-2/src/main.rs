765ec84059671d07baaa450d3749eefea7506a27
hah-love <legyx1@gmail.com>
2023-07-10 14:21:17 +0300

    rectangle area 3.0

--- rust_documentation/ch5/ch5-2/src/main.rs
+++ rust_documentation/ch5/ch5-2/src/main.rs
@@ -1,14 +1,25 @@
+struct Rectangle{
+    width: u32,
+    height: u32,
+} 
+
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50
+    };
+
+    // let rect1 = (30, 50);
     // let width1 = 30;
     // let height1 = 50;
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(dimensions: &Rectangle) -> u32 {
+    dimensions.width * dimensions.height
 }
