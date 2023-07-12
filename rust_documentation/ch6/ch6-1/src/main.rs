c295a9da7e556077debf89137d5cd06e8aa196df
hah-love <legyx1@gmail.com>
2023-07-12 11:56:49 +0300

    Option works

--- rust_documentation/ch6/ch6-1/src/main.rs
+++ rust_documentation/ch6/ch6-1/src/main.rs
@@ -1,17 +1,12 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
 
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
-    }
-}
 
 fn main() {
-    let x = Message::Quit;
-    x.call();
+    // let x: i8 = 5;
+    // let y: Option<i8> = Some(5);
+
+    // let sum = x + y;
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
 }
