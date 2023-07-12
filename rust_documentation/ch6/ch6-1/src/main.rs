ce1846e993e06a02665ad7543e0cc1eecc8b92dc
hah-love <legyx1@gmail.com>
2023-07-12 11:34:37 +0300

    create enum

--- rust_documentation/ch6/ch6-1/src/main.rs
+++ rust_documentation/ch6/ch6-1/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4,
+    V6,
 }
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
+
+fn route(ip_kind: IpAddrKind) {}
