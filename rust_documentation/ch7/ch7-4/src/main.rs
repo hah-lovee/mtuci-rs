7afb54ec60d11779ec4b6ce2afd3555ca5400787
hah-love <legyx1@gmail.com>
2023-07-12 17:34:48 +0300

    exceptions

--- rust_documentation/ch7/ch7-4/src/main.rs
+++ rust_documentation/ch7/ch7-4/src/main.rs
@@ -4,3 +4,14 @@ fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
 }
+
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {
+    // --snip--
+}
+
+fn function2() -> io::Result<()> {
+    // --snip--
+}
