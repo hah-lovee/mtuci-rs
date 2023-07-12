dd2d7c3144bc982ff1b68457c39406ff724cf41a
hah-love <legyx1@gmail.com>
2023-07-12 13:57:19 +0300

    pub struct

--- rust_documentation/ch7/restaurant/src/lib.rs
+++ rust_documentation/ch7/restaurant/src/lib.rs
@@ -21,15 +21,32 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
+
 
 
 fn deliver_order() {}
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // meal.seasonal_fruit = String::from("blueberries");
 }
