89ae3a9bb8170193cdf4c1c4e39ddedc8b69292d
hah-love <legyx1@gmail.com>
2023-07-12 11:40:47 +0300

    update enum 2.0

--- rust_documentation/ch6/ch6-1/src/main.rs
+++ rust_documentation/ch6/ch6-1/src/main.rs
@@ -1,14 +1,15 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddr::V4(127, 0, 0, 1);
 
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    route(home);
+    route(loopback);
 }
 
-fn route(ip_kind: IpAddrKind) {}
+fn route(ip_kind: IpAddr) {}
