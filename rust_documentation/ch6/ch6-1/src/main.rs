64e3695d3b78c144e9b9862ee37bce793cd1be44
hah-love <legyx1@gmail.com>
2023-07-12 11:46:34 +0300

    create impl for enum

--- rust_documentation/ch6/ch6-1/src/main.rs
+++ rust_documentation/ch6/ch6-1/src/main.rs
@@ -1,15 +1,17 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    route(home);
-    route(loopback);
+    let x = Message::Quit;
+    x.call();
 }
-
-fn route(ip_kind: IpAddr) {}
