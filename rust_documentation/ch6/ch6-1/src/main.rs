0e9fde090e2418701b57e43c5f897ee2af035d6b
hah-love <legyx1@gmail.com>
2023-07-12 11:57:43 +0300

    Option works 2.0

--- rust_documentation/ch6/ch6-1/src/main.rs
+++ rust_documentation/ch6/ch6-1/src/main.rs
@@ -1,12 +1,12 @@
 
 
 fn main() {
-    // let x: i8 = 5;
-    // let y: Option<i8> = Some(5);
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
-    // let sum = x + y;
-    let some_number = Some(5);
-    let some_char = Some('e');
+    let sum = x + y;
+    // let some_number = Some(5);
+    // let some_char = Some('e');
 
-    let absent_number: Option<i32> = None;
+    // let absent_number: Option<i32> = None;
 }
