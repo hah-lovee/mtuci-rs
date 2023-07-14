4215dc7763f80f3304819465f5880514e38a1cad
hah-love <legyx1@gmail.com>
2023-07-14 13:24:31 +0300

    concatenate str

--- rust_documentation/ch8/ch8-2/src/main.rs
+++ rust_documentation/ch8/ch8-2/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    let mut s = String::from("lo");
-    s.push('l');
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
 }
