ad47b29ac942769d02e6f0d577f1ecce5d20a0d9
hah-love <legyx1@gmail.com>
2023-07-14 13:27:34 +0300

    concatenate str 2.0

--- rust_documentation/ch8/ch8-2/src/main.rs
+++ rust_documentation/ch8/ch8-2/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
 }
