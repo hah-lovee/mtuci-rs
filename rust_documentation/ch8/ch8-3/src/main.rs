e6c01b806e766cd5a057891638eb1fc60fa3199a
hah-love <legyx1@gmail.com>
2023-07-14 13:42:49 +0300

    hashmap is owner

--- rust_documentation/ch8/ch8-3/src/main.rs
+++ rust_documentation/ch8/ch8-3/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
 }
