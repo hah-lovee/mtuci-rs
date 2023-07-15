10140cd1243a7958f82635a1f8fbd47a0930eab9
hah-love <legyx1@gmail.com>
2023-07-15 15:31:58 +0300

    generalized type in struct

--- rust_documentation/ch10/ch10-1/src/main.rs
+++ rust_documentation/ch10/ch10-1/src/main.rs
@@ -1,23 +1,10 @@
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
