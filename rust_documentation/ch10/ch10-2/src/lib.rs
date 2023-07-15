d6eb1095378afdcb1d3e9f5b46dc7da85ef29b67
hah-love <legyx1@gmail.com>
2023-07-15 16:17:23 +0300

    summarize_author

--- rust_documentation/ch10/ch10-2/src/lib.rs
+++ rust_documentation/ch10/ch10-2/src/lib.rs
@@ -1,17 +1,8 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
-}
+    fn summarize_author(&self) -> String;
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
     fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -23,7 +14,7 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
