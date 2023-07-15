d6bf56fdc0e8817a074b73f5f066c7b002e0b8f6
hah-love <legyx1@gmail.com>
2023-07-15 16:45:03 +0300

    lifetime in impl

--- rust_documentation/ch10/ch10-3/src/main.rs
+++ rust_documentation/ch10/ch10-3/src/main.rs
@@ -1,17 +1,24 @@
-fn main() {
-    let string1 = String::from("long string is long");
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
 
-    {
-        let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        println!("The longest string is {}", result);
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
     }
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
     }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
