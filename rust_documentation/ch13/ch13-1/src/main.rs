f5b534f43c3308c5aa3e5d18f246f729e607f39d
hah-love <legyx1@gmail.com>
2023-07-19 17:17:22 +0300

    create store shirts

--- rust_documentation/ch13/ch13-1/src/main.rs
+++ rust_documentation/ch13/ch13-1/src/main.rs
@@ -1,3 +1,45 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn getaway (&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.counter())
+    }
+
+    fn counter(&self) -> ShirtColor {
+        let mut red_count = 0;
+        let mut blue_count = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => red_count += 1,
+                ShirtColor::Blue => blue_count += 1,
+            }
+        }
+        if red_count > blue_count {
+            ShirtColor::Red
+        } else { ShirtColor::Blue }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec!(ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red)
+    };
+
+    let user1 = Some(ShirtColor::Red);
+    let user2 = None;
+
+    let result_1 = store.getaway(user1);
+    let result_2 = store.getaway(user2);
+
+    println!("user1 -> {:?}, user2 -> {:?}", result_1, result_2);
+
 }
