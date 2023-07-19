803a70af59356af0dacea3b7e84ff91934ce4c09
hah-love <legyx1@gmail.com>
2023-07-19 17:26:53 +0300

    borrow in closure

--- rust_documentation/ch13/ch13-1/src/main.rs
+++ rust_documentation/ch13/ch13-1/src/main.rs
@@ -1,45 +1,10 @@
-#[derive(Debug, PartialEq, Copy, Clone)]
-enum ShirtColor {
-    Red,
-    Blue,
-}
-
-struct Inventory {
-    shirts: Vec<ShirtColor>,
-}
-
-impl Inventory {
-    fn getaway (&self, user_preference: Option<ShirtColor>) -> ShirtColor {
-        user_preference.unwrap_or_else(|| self.counter())
-    }
-
-    fn counter(&self) -> ShirtColor {
-        let mut red_count = 0;
-        let mut blue_count = 0;
-
-        for color in &self.shirts {
-            match color {
-                ShirtColor::Red => red_count += 1,
-                ShirtColor::Blue => blue_count += 1,
-            }
-        }
-        if red_count > blue_count {
-            ShirtColor::Red
-        } else { ShirtColor::Blue }
-    }
-}
-
 fn main() {
-    let store = Inventory {
-        shirts: vec!(ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red)
-    };
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
 
-    let user1 = Some(ShirtColor::Red);
-    let user2 = None;
+    let only_borrows = || println!("From closure: {:?}", list);
 
-    let result_1 = store.getaway(user1);
-    let result_2 = store.getaway(user2);
-
-    println!("user1 -> {:?}, user2 -> {:?}", result_1, result_2);
-
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
 }
