23fa6993e5964a9556436579a4930b9c20bf0b6b
hah-love <legyx1@gmail.com>
2023-07-19 17:28:28 +0300

    borrow mut in closure

--- rust_documentation/ch13/ch13-1/src/main.rs
+++ rust_documentation/ch13/ch13-1/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let list = vec![1, 2, 3];
+    let mut list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
-    let only_borrows = || println!("From closure: {:?}", list);
+    let mut borrows_mutably = || list.push(7);
 
-    println!("Before calling closure: {:?}", list);
-    only_borrows();
+    borrows_mutably();
     println!("After calling closure: {:?}", list);
 }
