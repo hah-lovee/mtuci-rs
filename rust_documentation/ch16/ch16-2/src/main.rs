7e668edde0b4fa1fd92156b402644df381d83390
hah-love <legyx1@gmail.com>
2023-07-20 15:01:04 +0300

    more thread

--- rust_documentation/ch16/ch16-2/src/main.rs
+++ rust_documentation/ch16/ch16-2/src/main.rs
@@ -6,6 +6,8 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -15,10 +17,25 @@ fn main() {
         ];
 
         for val in vals {
-            tx.send(val).unwrap();
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hello"),
+            String::from("fromm"),
+            String::from("other"),
+            String::from("threadd")
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1))
+        }
+    });
+
 
     for received in rx{
         println!("Got: {}", received);
